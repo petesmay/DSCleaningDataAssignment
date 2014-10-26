9cc12b6a64a458960bdcf5cefde4b88711c851ab
Peter May <Pete.S.May@gmail.com>
2014-10-26 11:06:50 +0000

    Updated Code Book. Steps 3 and 4 complete

--- CodeBook.md
+++ CodeBook.md
@@ -4,9 +4,23 @@ The code book should contain:
 3. Information about the experimental study design you used
 
 
-Study Design
-=========
+## Study Design
 
+### Summary Choices
 
-Code Book
-=========
+#### Mean and Standard Deviation selection
+Only measurements on the mean and standard deviation for each variable are provided in the tidy data set. These are extracted based on the names in the (originally supplied) features.txt file. 
+
+Variable names with "mean()" and "std()" are the only variables included, as features_info.txt declares these to represent the mean and standard deviation values. 
+
+Variable names with "meanFreq()" are not included as these represent "weighted average of the frequency components to obtain a mean frequency".
+
+Any other mention of "mean" or "Mean" is also ignored and not included.
+
+#### Subject and Test/Training Label Variables
+Both these sets of data are included in the original merge of the data sets (see the MergeData function).
+
+#### Variable Labels
+Variables are labelled using CamelCase to aid readability due to long expanded names.
+
+## Code Book
--- run_analysis.R
+++ run_analysis.R
@@ -65,24 +65,108 @@ MergeData = function(){
   return (merged)
 }
 
-ExtractMeanStd <- function(data){
+GetMeanStdIndexes <- function(){
+  # Returns the indexes of all rows from features.txt which include "mean()"
+  # or "std()".
+  # Args: none
+  # Returns:
+  #   An integer vector of row indexes from features.txt including "mean()" or
+  #   "std()" in the feature label
+  # Side effects:
+  #   features is made globally available
+  features <<- read.table("./data/UCI HAR Dataset/features.txt")
+  indexes <- grep("(mean|std)\\(\\)", features$V2)
+  return (indexes)
+}
+
+ExtractMeanAndStd <- function(data){
   # Extracts the means and standard deviations for each measurement that
-  # include "mean()" or "std()" in its feature name
+  # include "mean()" or "std()" in its feature name. Names with "mean"
+  # without the "()" are not included (see CodeBook.md for details).
+  # The subject who performed the test, and the test type are also included
+  # in the output data set.
   # Args: data
   #   The data set to extract the means and std columns from
   # Returns:
-  #   The reduced data set including only mean and std columns
-  features <- read.table("./data/UCI HAR Dataset/features.txt")
-  indexes <- grep("(mean|std)\\(\\)", features$V2)
+  #   The reduced data set including subject, test type, mean and std variables
+  indexes <- GetMeanStdIndexes()
+  indexes <- indexes+2          # Add 2 to each index to skip first 2 columns
+  indexes <- c(1:2, indexes)    # subject; test type; mean|std variables
   
-  reduced <- data[, indexes+2]    # +2 to ignore first 2 columns
+  reduced <- data[, indexes]    # subset only those columns
   return (reduced)
+}
+
+DescribeActivities <- function(data){
+  # Replaces the test type (i.e. activity) with the associated label from
+  # activity_labels.txt
+  # Args: data
+  #   The data set to operate over
+  # Returns:
+  #   The modified data set with test activity replaced with an appropriate
+  #   label
+  
+  activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+  labels <- tolower(activities$V2)
+  
+  data$V1.1 <- as.factor(data$V1.1)  # turn the current types into a factor
+  levels(data$V1.1) <- labels        # use label to rebase the factor
+  
+  return (data)
+}
+
+LabelData <- function(data){
+  # Labels the variables appropriately from the names in the features.txt 
+  # file.
+  # Abbreviations are expanded out
+  # Hyphens and brackets are removed.
+  # CamelCase is used to improve readability in the long names
+  # Args: data
+  #   The data set to label
+  # Returns:
+  #   The CamelCase labelled data set
+  
+  # get the relevant labels from features.txt
+  indexes <- GetMeanStdIndexes()
+  colnames <- as.character(features[indexes, 2])
+  
+  # remove duplicate "Body", e.g. "fBodyBodyAccJerkMag"
+  colnames <- sub("(fBody)Body", "\\1", colnames)
+  # remove ()'s
+  colnames <- sub("\\(\\)", "", colnames)
+  # remove -'s
+  colnames <- sub("\\-", "", colnames)
+  
+  # expand "Acc" to "Acceleration"
+  colnames <- sub("Acc", "Acceleration", colnames)
+  # expand initial "t"s and "f"s to "Time" and "Frequency
+  colnames <- sub("^t", "Time", colnames)
+  colnames <- sub("^f", "Frequency", colnames)
+  # "mean" is capitalised (for readability)
+  colnames <- sub("mean", "Mean", colnames)
+  # "std" is expanded to "StandardDeviation"
+  colnames <- sub("std", "StandardDeviation", colnames)
+  # "Gyro" is expanded to "Gyroscope"
+  colnames <- sub("Gyro", "Gyroscope", colnames)
+  # "Mag" is expanded to "Magnitude", colnames)
+  colnames <- sub("Mag", "Magnitude", colnames)
+  # Final "X", "Y", or "Z" is expanded to "XAxis" or similar
+  colnames <- sub("([XYZ])$", "\\1Axis", colnames)
+  
+  # prepend Subject, Activity
+  colnames <- c("Subject", "Activity", colnames)  
+  
+  # Finally set the names
+  names(data) <- colnames
+  return (data)
 }
 
 #############################################
 ## Process Raw data
 PkgInstall("data.table")
 
-DownloadRaw()                     # Download raw data
-merged <- MergeData()             # Merge the data
-merged <- ExtractMeanStd(merged)  # Extract means and std
+DownloadRaw()                        # Download raw data
+merged <- MergeData()                # Step 1: Merge the data
+merged <- ExtractMeanAndStd(merged)  # Step 2: Extract means and std
+merged <- DescribeActivities(merged) # Step 3: Use descriptive activity names
+merged <- LabelData(merged)          # Step 4: Label variables
